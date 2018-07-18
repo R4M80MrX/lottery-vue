7eb3db289a51372abd52207cb5baa84cc0e01227
wustmz <865011721@qq.com>
2018-07-18 17:50:57 +0800

    update readme

--- src/pages/common/TitleBar.vue
+++ src/pages/common/TitleBar.vue
@@ -30,7 +30,7 @@
 </script>
 
 <style scoped>
-  .div_back{
+  .div_back {
     position: absolute;
     left: 15px;
   }
---
+++ src/pages/shopping/CartPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "cart-page"
+    }
+</script>
+
+<style scoped>
+
+</style>
